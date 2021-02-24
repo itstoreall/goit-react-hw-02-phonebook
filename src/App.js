1e310246bce566fb025c1914d1c5fd2e37d1f940
Serhii Stanislav <serhiistanislav@gmail.com>
2021-02-25 01:07:57 +0200

    adds an initial form markup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import React, { Fragment, useState } from 'react';
+
+// const state = {
+//   contacts: [],
+//   name: '',
+// };
+
 function App() {
-  return <p>Hi</p>;
+  const [count, setCount] = useState([]);
+
+  const handleAddContact = e => {
+    console.log(e.target.value);
+  };
+
+  return (
+    <Fragment>
+      <p>{count}</p>
+      <form>
+        <input type="text" onChange={handleAddContact} />
+        <button type="button" onClick={handleAddContact}>
+          Добавить
+        </button>
+      </form>
+      <span></span>
+    </Fragment>
+  );
 }
 
 export default App;
