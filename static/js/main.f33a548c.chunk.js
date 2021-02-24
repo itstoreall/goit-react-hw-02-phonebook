0458b7a02506664963ef07c4852830d82ac8965b
Serhii Stanislav <serhiistanislav@gmail.com>
2021-02-24 20:30:35 +0200

    Updates

---
+++ static/js/main.f33a548c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-02-phonebook"]=this["webpackJsonpgoit-react-hw-02-phonebook"]||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var o=n(1),c=n.n(o),r=n(3),i=n.n(r),s=n(0);var h=function(){return Object(s.jsx)("p",{children:"Hi"})};n(9);i.a.render(Object(s.jsx)(c.a.StrictMode,{children:Object(s.jsx)(h,{})}),document.getElementById("root"))}},[[10,1,2]]]);
+//# sourceMappingURL=main.f33a548c.chunk.js.map
