46f1943bb45a1087bdca261093d3b1a662022b10
Serhii Stanislav <serhiistanislav@gmail.com>
2021-02-27 20:00:23 +0200

    adds a baseData

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React, { useState } from 'react';
 import Form from './components/PhonebookForm';
 import ContactList from './components/ContactList';
+import baseData from './components/Data/baseData.json';
 import { wrapper, title, subtitle } from './App.module.scss';
 
-// const state = {
-//   contacts: [],
-//   name: '',
-// };
-
 const App = () => {
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState(baseData);
   const [filter, setFilter] = useState('');
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
 
-  // Записывает значение инпута в стейт
+  // Записывает значения инпута в стейт
   const handleInputChange = e => {
     const { name, value } = e.target;
-    console.log(value);
+
     switch (name) {
       case 'name':
         setName(value);
@@ -33,15 +29,16 @@ const App = () => {
     }
   };
 
+  // Добавление нового контакта в стате
+  const handleSubmit = newContact => {
+    setContacts(prevState => [newContact, ...prevState]);
+  };
+
+  // Фильтрация контактов
   const normalizedFilter = filter.toLowerCase();
   const filteredContacts = contacts.filter(contact =>
     contact.name.toLowerCase().includes(normalizedFilter),
   );
-
-  const handleSubmit = newContact => {
-    console.log(newContact);
-    setContacts(prevState => [newContact, ...prevState]);
-  };
 
   return (
     <div className={wrapper}>
